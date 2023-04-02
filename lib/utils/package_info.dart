5523702c60b55ee04db012dfb665027a4c5b2111
ilgnefz <ilgnefz@foxmail.com>
2023-04-02 20:06:41 +0800

    :bug: Fixed version display bug

--- lib/utils/package_info.dart
+++ lib/utils/package_info.dart
@@ -11,5 +11,10 @@ class PackageDesc {
     _packageInfo = await PackageInfo.fromPlatform();
   }
 
-  static String getVersion() => _packageInfo.version;
+  static String getVersion() {
+    String number = _packageInfo.buildNumber;
+    String version = _packageInfo.version;
+    version = number == '0' ? version : '$version.$number';
+    return version;
+  }
 }
