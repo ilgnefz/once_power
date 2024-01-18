54ceac78a4d92994b8a4e5c4afc7e3bfe7dfd90e
ilgnefz <ilgnefz@foxmail.com>
2024-01-18 19:53:53 +0800

    :bug: Added clear cache before closing

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:once_power/global.dart';
 import 'package:once_power/views/action_bar/action_bar.dart';
 import 'package:once_power/views/bottom_bar/bottom_bar.dart';
 import 'package:once_power/views/content_bar/content_bar.dart';
+import 'package:window_manager/window_manager.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with WindowListener {
+  @override
+  void initState() {
+    super.initState();
+    windowManager.addListener(this);
+    _init();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+  void _init() async {
+    await windowManager.setPreventClose(true);
+    setState(() {});
+  }
+
+  @override
+  void onWindowClose() async {
+    saveOrNo();
+    await windowManager.destroy();
+  }
 
   @override
   Widget build(BuildContext context) {
