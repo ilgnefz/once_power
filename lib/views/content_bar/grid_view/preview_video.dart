a672dc1877598426fe3c72a30c5215df709b1510
ilgnefz <ilgnefz@foxmail.com>
2025-03-22 17:44:02 +0800

    :lipstick: Optimized the progress bar for preview videos

--- lib/views/content_bar/grid_view/preview_video.dart
+++ lib/views/content_bar/grid_view/preview_video.dart
@@ -6,6 +6,7 @@ import 'package:once_power/utils/format.dart';
 import 'package:once_power/views/content_bar/grid_view/play_btn.dart';
 import 'package:video_player/video_player.dart';
 
+import 'custom_video_progress_indicator.dart';
 import 'err_image.dart';
 import 'loading_image.dart';
 
@@ -55,35 +56,37 @@ class _PreviewVideoState extends State<PreviewVideo> {
       if (_controller.value.hasError) {
         return Center(child: ErrorImage(isPreview: true, file: widget.file));
       } else {
-        return InkWell(
-          onTap: () => Navigator.pop(context),
-          child: AspectRatio(
-            aspectRatio: _controller.value.aspectRatio,
-            child: Column(
+        return Column(
+          children: [
+            Expanded(
+              child: InkWell(
+                onTap: () => Navigator.pop(context),
+                child: AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                ),
+              ),
+            ),
+            Row(
               children: [
-                Expanded(child: VideoPlayer(_controller)),
-                Row(
-                  children: [
-                    PlayBtn(controller: _controller),
-                    Expanded(
-                      child: VideoProgressIndicator(
-                        _controller,
-                        allowScrubbing: true,
-                        padding: EdgeInsets.zero,
-                        colors: VideoProgressColors(
-                          playedColor: Theme.of(context).colorScheme.primary,
-                          backgroundColor: Colors.white,
-                        ),
-                      ),
+                PlayBtn(controller: _controller),
+                Expanded(
+                  child: CustomVideoProgressIndicator(
+                    _controller,
+                    allowScrubbing: true,
+                    padding: EdgeInsets.zero,
+                    colors: VideoProgressColors(
+                      playedColor: Theme.of(context).colorScheme.primary,
+                      backgroundColor: Colors.white,
                     ),
-                    SizedBox(width: AppNum.largeG),
-                    Text(timeLine, style: TextStyle(color: Colors.white)),
-                    SizedBox(width: AppNum.largeG),
-                  ],
-                )
+                  ),
+                ),
+                SizedBox(width: AppNum.largeG),
+                Text(timeLine, style: TextStyle(color: Colors.white)),
+                SizedBox(width: AppNum.largeG),
               ],
             ),
-          ),
+          ],
         );
       }
     }
