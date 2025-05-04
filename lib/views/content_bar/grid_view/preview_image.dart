d81d239e594576f656dea719bbb87647368dd92f
ilgnefz <ilgnefz@foxmail.com>
2025-05-04 13:01:55 +0800

    :bug: Fixed the issue of blurry image cache display

--- lib/views/content_bar/grid_view/preview_image.dart
+++ lib/views/content_bar/grid_view/preview_image.dart
@@ -1,15 +1,16 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:once_power/views/content_bar/grid_view/loading_image.dart';
 
 import 'err_image.dart';
-import 'loading_image.dart';
 
 class PreviewImage extends StatelessWidget {
   const PreviewImage({super.key, required this.id, required this.file});
 
   final String id;
   final String file;
+
   @override
   Widget build(BuildContext context) {
     return InteractiveViewer(
@@ -20,7 +21,6 @@ class PreviewImage extends StatelessWidget {
         File(file),
         key: ValueKey(file),
         fit: BoxFit.scaleDown,
-        cacheHeight: MediaQuery.of(context).size.height.toInt(),
         errorBuilder: (context, exception, stackTrace) =>
             ErrorImage(isPreview: true, file: file),
         frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
@@ -28,9 +28,27 @@ class PreviewImage extends StatelessWidget {
             return child;
           } else {
             return AnimatedSwitcher(
-              duration: const Duration(milliseconds: 200),
-              child:
-                  frame != null ? child : const LoadingImage(isPreview: true),
+              duration: Duration.zero,
+              child: frame != null
+                  ? child
+                  : Image.file(
+                      File(file),
+                      key: ValueKey(file),
+                      fit: BoxFit.scaleDown,
+                      cacheHeight: MediaQuery.of(context).size.height.toInt(),
+                      frameBuilder:
+                          (context, child, frame, wasSynchronouslyLoaded) {
+                        if (wasSynchronouslyLoaded) {
+                          return child;
+                        } else {
+                          return AnimatedSwitcher(
+                              duration: const Duration(milliseconds: 1),
+                              child: frame != null
+                                  ? child
+                                  : const LoadingImage(isPreview: true));
+                        }
+                      },
+                    ),
             );
           }
         },
