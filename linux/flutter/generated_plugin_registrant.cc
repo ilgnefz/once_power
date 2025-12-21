1880effdc7ac855f35ab632c10a19c3ac050bc28
ilgnefz <ilgnefz@foxmail.com>
2025-12-21 10:00:19 +0800

    :rocket: Add Linux support

---
+++ linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,47 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <desktop_drop/desktop_drop_plugin.h>
+#include <file_selector_linux/file_selector_plugin.h>
+#include <flutter_avif_linux/flutter_avif_linux_plugin.h>
+#include <flutter_media_info/flutter_media_info_plugin.h>
+#include <fvp/fvp_plugin.h>
+#include <screen_retriever_linux/screen_retriever_linux_plugin.h>
+#include <tray_manager/tray_manager_plugin.h>
+#include <url_launcher_linux/url_launcher_plugin.h>
+#include <window_manager/window_manager_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) desktop_drop_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "DesktopDropPlugin");
+  desktop_drop_plugin_register_with_registrar(desktop_drop_registrar);
+  g_autoptr(FlPluginRegistrar) file_selector_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FileSelectorPlugin");
+  file_selector_plugin_register_with_registrar(file_selector_linux_registrar);
+  g_autoptr(FlPluginRegistrar) flutter_avif_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterAvifLinuxPlugin");
+  flutter_avif_linux_plugin_register_with_registrar(flutter_avif_linux_registrar);
+  g_autoptr(FlPluginRegistrar) flutter_media_info_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterMediaInfoPlugin");
+  flutter_media_info_plugin_register_with_registrar(flutter_media_info_registrar);
+  g_autoptr(FlPluginRegistrar) fvp_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FvpPlugin");
+  fvp_plugin_register_with_registrar(fvp_registrar);
+  g_autoptr(FlPluginRegistrar) screen_retriever_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "ScreenRetrieverLinuxPlugin");
+  screen_retriever_linux_plugin_register_with_registrar(screen_retriever_linux_registrar);
+  g_autoptr(FlPluginRegistrar) tray_manager_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "TrayManagerPlugin");
+  tray_manager_plugin_register_with_registrar(tray_manager_registrar);
+  g_autoptr(FlPluginRegistrar) url_launcher_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "UrlLauncherPlugin");
+  url_launcher_plugin_register_with_registrar(url_launcher_linux_registrar);
+  g_autoptr(FlPluginRegistrar) window_manager_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "WindowManagerPlugin");
+  window_manager_plugin_register_with_registrar(window_manager_registrar);
+}
